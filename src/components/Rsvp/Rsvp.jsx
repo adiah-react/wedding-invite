a6b3816c334c740e7e265b59cc95f1fea98419f3
Nathaniel Adiah <nathanieladiah@gmail.com>
2025-07-27 14:22:48 -0400

    Layout the form

--- src/components/Rsvp/Rsvp.jsx
+++ src/components/Rsvp/Rsvp.jsx
@@ -22,7 +22,46 @@ const Rsvp = () => {
       </div>
 
       <div className="bottom">
-        <div className="form"></div>
+        <div className="form">
+          <form action="">
+            <div className="form-group">
+              <label htmlFor="message">Message</label>
+              <textarea name="" id="message" rows="4"></textarea>
+            </div>
+            {/* <div className="form-group">
+              <label htmlFor="attendance">Attendance</label>
+            </div> */}
+            <fieldset>
+              <legend>Will you be attending?</legend>
+              <div>
+                <input
+                  type="radio"
+                  id="attendanceChoice1"
+                  name="attendance"
+                  value="yes"
+                />
+                <label for="attendanceChoice1">Yes</label>
+
+                <input
+                  type="radio"
+                  id="attendanceChoice2"
+                  name="attendance"
+                  value="no"
+                />
+                <label for="attendanceChoice2">No</label>
+
+                <input
+                  type="radio"
+                  id="attendanceChoice3"
+                  name="attendance"
+                  value="maybe"
+                />
+                <label for="attendanceChoice3">Maybe</label>
+              </div>
+            </fieldset>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
         <div className="thanks"></div>
       </div>
     </div>
