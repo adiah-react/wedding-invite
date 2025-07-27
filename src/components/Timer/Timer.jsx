f616190a12b43b7bd5c6dd5a674ce5e9fbd1692f
Nathaniel Adiah <nathanieladiah@gmail.com>
2025-07-27 01:40:39 -0400

    Layout the countdown

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,13 +1,27 @@
 import Countdown from "react-countdown";
+import "./timer.scss";
 
 const Timer = () => {
   const date = "2026-06-20T14:00:00";
+
+  // const renderer = ({ days, hours, minutes, seconds, completed }) => {
+  //   if (completed) {
+  //     return <p>Completed</p>;
+  //   } else {
+  //     return (
+  //       <span>
+  //         {days}:{hours}:{minutes}:{seconds}
+  //       </span>
+  //     );
+  //   }
+  // };
   return (
     <div className="timer">
-      <p>
+      <p className="title">
         Time left until <span>the</span> wedding
       </p>
-      <Countdown date={date} />
+
+      <div className="countdown">{<Countdown date={date} />}</div>
       <div className="labels">
         <span>days</span>
         <span>hours</span>
