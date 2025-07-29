8ef7521b32d9d1257ea3d54d7f5f6d9c3977b09b
Nathaniel Adiah <nathanieladiah@gmail.com>
2025-07-29 09:29:33 -0400

    Style the Hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
       <div className="image"></div>
       <div className="text">
         <motion.p
-          className="subheading"
+          className="subheading frosted"
           initial={{ x: -100, opacity: 0 }}
           whileInView={{ x: 0, opacity: 1 }}
           transition={{ duration: 0.8, ease: "easeOut" }}
